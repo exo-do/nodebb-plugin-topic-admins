e7c33a6814b72abed08814ba4716dfa92ed5228b
alberto__segura <alberto.segura.delgado@gmail.com>
2015-03-09 13:36:21 +0100

    Add and delete admins with UI. Buttons only are shown in main post of a topic

--- library.js
+++ library.js
@@ -13,9 +13,9 @@ var SocketPlugins = module.parent.require('./socket.io/plugins');
 var topicAdmins = {};
 
 
-topicAdmins.havePermission = function(user, content)
+topicAdmins.havePermission = function(user, admins)
 {
-  // Check if user have permission to edit post
+  /* Check if user have permission to edit post
   var admins = content.match(/\[admins\].*\[\/admins\]/i);
   if(admins)
   {
@@ -30,6 +30,10 @@ topicAdmins.havePermission = function(user, content)
     }
   }
   return false;
+  */
+
+  return ( admins && (JSON.parse(admins)).indexOf(user) > -1);
+
 };
 
 SocketPlugins.editMainPost = function(socket, data, callback) {
@@ -40,7 +44,8 @@ SocketPlugins.editMainPost = function(socket, data, callback) {
   {
     User.getUserData(socket.uid, function(err, user){
       Posts.getPostData(data.pid, function(err, post){
-        if(post.uid == socket.uid || topicAdmins.havePermission(user.username, post.content))
+        var admins = post.admins || "[]";
+        if(post.uid == socket.uid || topicAdmins.havePermission(user.uid, admins))
         {
           /*
           Posts.setPostFields(data.pid, {title:data.title, content:data.content}, function(err, r){
@@ -62,6 +67,117 @@ SocketPlugins.editMainPost = function(socket, data, callback) {
   {
     callback("noPermission", "");
   }
+};
+
+SocketPlugins.addTopicAdmin = function(socket, data, callback) {
+  // i receive post edition!
+  //console.log(data);
+
+  if(socket.uid && data.user)
+  {
+    User.getUserData(socket.uid, function(err, user){
+      Posts.getPostData(data.pid, function(err, post){
+        if(post.uid == socket.uid || topicAdmins.havePermission(user.uid, post.admins))
+        {
+          var actAdmins = post.admins || "[]";
+          actAdmins = JSON.parse(actAdmins);
+          User.getUidByUsername(data.user, function(err, nAdmin){
+            if(err || !nAdmin)
+            {
+              callback("noUser", "");
+              return;
+            }
+            actAdmins.push(nAdmin);
+            Posts.setPostFields(data.pid, {admins:JSON.stringify(actAdmins)}, function(err, r){
+              callback(err, "ok");
+            });
+          });
+        }
+        else
+        {
+          callback("noPermission", "");
+        }
+      });
+    });
+  }
+  else
+  {
+    callback("noPermission", "");
+  }
+};
+
+SocketPlugins.deleteTopicAdmin = function(socket, data, callback) {
+  // i receive post edition!
+  //console.log(data);
+
+  if(socket.uid && data.uid && data.pid)
+  {
+    User.getUserData(socket.uid, function(err, user){
+      Posts.getPostData(data.pid, function(err, post){
+        if(post.uid == socket.uid || topicAdmins.havePermission(user.uid, post.admins))
+        {
+          var actAdmins = post.admins || "[]";
+          actAdmins = JSON.parse(actAdmins);
+          if(err)
+          {
+            callback("noUser", "");
+            return;
+          }
+          var deleteAdmin = actAdmins.indexOf(data.uid);
+          actAdmins.splice(deleteAdmin, 1);
+          Posts.setPostFields(data.pid, {admins:JSON.stringify(actAdmins)}, function(err, r){
+            callback(err, "ok");
+          });
+        }
+        else
+        {
+          callback("noPermission", "");
+        }
+      });
+    });
+  }
+  else
+  {
+    callback("noPermission", "");
+  }
+};
+
+SocketPlugins.getTopicAdmin = function(socket, data, callback){
+  Posts.getPostData(data.pid, function(err, post){
+    //console.log(post);
+    if(post.admins)
+    {
+      User.getUsersData(JSON.parse(post.admins), function(err, admins){
+        if(err)
+        {
+          callback("err", "");
+          return;
+        }
+        else
+        {
+          callback(null, admins);
+        }
+      });
+    }
+    else
+    { // No admins
+      callback(null, []);
+    }
+  });
+};
+
+SocketPlugins.isTopicAdmin = function(socket, data, callback){
+  Posts.getPostData(data.pid, function(err, post){
+    var admins = post.admins || "[]";
+    if(post.uid == socket.uid || topicAdmins.havePermission(socket.uid, admins) )
+    {
+      callback(null, true);
+    }
+    else
+    {
+      callback(null, false);
+    }
+  });
 };
 
 module.exports = topicAdmins;
--- public/js/client.js
+++ public/js/client.js
@@ -9,56 +9,182 @@
 				S = String;
 				// Do something
 				var editBtn = '<button class="btn btn-sm btn-default edit" type="button" title="Edit" id="topicAdminsEdit"><i class="fa fa-pencil"></i> Editar</button>';
-				if($(".topic-buttons.clearfix"))
+				var adminsBtn = '<button class="btn btn-sm btn-default" type="button" title="Admins" id="topicAdminsShow">Admins</button>';
+				// Get posts
+				var posts = $(".post-row");
+				// Load Composer to edit post
+				var pid = $(posts[0]).attr("data-pid");
+				if(pid && $(".topic-buttons.clearfix"))
 				{
-					$(".topic-buttons.clearfix").append(editBtn);
-					$("#topicAdminsEdit").on("click", function()
-					{
-						// Get posts
-						var posts = $(".post-row");
-						// Load Composer to edit post
-						var pid = $(posts[0]).attr("data-pid");
-						composer.editPost(pid);
-						setTimeout(function(){
-							var submitBtn = $(".composer-container").find(".btn.btn-primary");
-							submitBtn.attr("data-action", "editTopicAdmins");
-							submitBtn.on("click", function(){
-								var c = $(".write").val();
-								var t = $(".title.form-control").val();
-								data = {pid:pid, title:t, content:c};
-								socket.emit("plugins.editMainPost", data, function(err, res){
-									if(!err)
-									{
-										app.alert({
-											type: 'success',
-											timeout: 3000,
-											title: 'Editado',
-											message: "El post fue editado correctamente!",
-											alert_id: 'post_success'
+					$(posts[0]).find(".topic-buttons.clearfix").append(adminsBtn);
+					$("#topicAdminsShow").on("click", createAdminEditor);
+					socket.emit("plugins.isTopicAdmin", {pid:pid}, function(err, isTopicAdmin){
+						console.log(isTopicAdmin);
+						if($(".topic-buttons.clearfix") && isTopicAdmin)
+						{
+							$(posts[0]).find(".topic-buttons.clearfix").append(editBtn);
+							$("#topicAdminsEdit").on("click", function()
+							{
+								// Get posts
+								var posts = $(".post-row");
+								// Load Composer to edit post
+								var pid = $(posts[0]).attr("data-pid");
+								composer.editPost(pid);
+								setTimeout(function(){
+									var submitBtn = $(".composer-container").find(".btn.btn-primary");
+									submitBtn.attr("data-action", "editTopicAdmins");
+									submitBtn.on("click", function(){
+										var c = $(".write").val();
+										var t = $(".title.form-control").val();
+										data = {pid:pid, title:t, content:c};
+										socket.emit("plugins.editMainPost", data, function(err, res){
+											if(!err)
+											{
+												app.alert({
+													type: 'success',
+													timeout: 3000,
+													title: 'Editado',
+													message: "El post fue editado correctamente!",
+													alert_id: 'post_success'
+												});
+											}
+											else
+											{
+												app.alert({
+													type: 'danger',
+													timeout: 3000,
+													title: 'Error',
+													message: "Hubo un error al editar! Quizás no tengas sufucuentes permisos.",
+													alert_id: 'post_error'
+												});
+											}
+											var compid = $($(".composer")[0]).attr("id");
+											compid = compid.replace("cmp-uuid-");
+											composer.minimize(compid);
 										});
-									}
-									else
-									{
-										app.alert({
-											type: 'danger',
-											timeout: 3000,
-											title: 'Error',
-											message: "Hubo un error al editar! Quizás no tengas sufucuentes permisos.",
-											alert_id: 'post_error'
-										});
-									}
-									var compid = $($(".composer")[0]).attr("id");
-									compid = compid.replace("cmp-uuid-");
-									composer.minimize(compid);
-								});
-							});
-						}, 300);
-					})
+									});
+								}, 300);
+							})
+						}
+					});
 				}
 
 			});
 		});
 		
+	}
+
+	addNewAdmin = function()
+	{
+		var user = $("#newAdmin").val();
+		var posts = $(".post-row");
+		var pid = $(posts[0]).attr("data-pid");
+		socket.emit("plugins.addTopicAdmin", {user:user, pid:pid}, function(err, res){
+			if(!err)
+			{
+				app.alert({
+					type: 'success',
+					timeout: 3000,
+					title: 'Añadido',
+					message: "Admin añadido correctamente!",
+					alert_id: 'post_success'
+				});
+				getTopicAdmin();
+			}
+			else
+			{
+				app.alert({
+					type: 'danger',
+					timeout: 3000,
+					title: 'Error',
+					message: "Hubo un error al añadir este admin! Quizás no tengas sufucuentes permisos.",
+					alert_id: 'post_error'
+				});
+			}
+		});
+	}
+
+	getTopicAdmin = function()
+	{
+		var user = $("#newAdmin").val();
+		var posts = $(".post-row");
+		var pid = $(posts[0]).attr("data-pid");
+		socket.emit("plugins.getTopicAdmin", {pid:pid}, function(err, res){
+			if(!err)
+			{
+				console.log(res);
+				$("#actAdmins").html(""); // Clear admins..
+				for(var i=0;i<res.length;i++)
+				{
+					$("#actAdmins").append("<p style='display:inline;'><img src='"+res[i].picture+"' width='60' height='60'><a href='/user/"+res[i].userslug+"'><h3>"+res[i].username+"</h3></a> (<a href='#' onclick='deleteAdmin("+res[i].uid+")'>Eliminar</a>) </p>");
+				}
+			}
+			else
+			{
+				app.alert({
+					type: 'danger',
+					timeout: 3000,
+					title: 'Error',
+					message: "Hubo un error al obtener los admins!",
+					alert_id: 'post_error'
+				});
+			}
+		});
+	}
+
+	deleteAdmin = function(uid)
+	{
+		var user = $("#newAdmin").val();
+		var posts = $(".post-row");
+		var pid = $(posts[0]).attr("data-pid");
+		socket.emit("plugins.deleteTopicAdmin", {pid:pid, uid:uid}, function(err, res){
+			if(!err)
+			{
+				app.alert({
+					type: 'success',
+					timeout: 3000,
+					title: 'Eliminado!',
+					message: "Admin eliminado",
+					alert_id: 'post_error'
+				});
+			}
+			else
+			{
+				app.alert({
+					type: 'danger',
+					timeout: 3000,
+					title: 'Error',
+					message: "Hubo un error al eliminar los admins!",
+					alert_id: 'post_error'
+				});
+			}
+			getTopicAdmin();
+		});
+	}
+
+	createAdminEditor = function()
+	{
+		var template = "Añadir nuevo administrador: <input type='text' id='newAdmin' placeholder='Nombre de Usuario'><button id='addNewAdmin' onclick='addNewAdmin()'>Añadir</button><br><h1>Administradores:</h1><div id='actAdmins'></div><script>getTopicAdmin();</script>";
+		bootbox.dialog({
+			title: 'Administradores del topic',
+			message: template,
+			buttons: {
+				cancel: {
+					label: 'Cancelar',
+					className: 'btn-default',
+					callback: function (e) {
+						return true;
+					}
+				},
+				save: {
+					label: 'Aceptar',
+					className: 'btn-primary',
+					callback: function (e) {
+						return addNewAdmin();
+					}
+				}
+			}
+		}, getTopicAdmin);
 	}
 
 	init();
